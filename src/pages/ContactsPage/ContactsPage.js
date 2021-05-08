edfb89034718aee073b482d5c4bf6ec6a389fa1f
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-05-08 21:29:31 +0300

    убирает лишнее

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -6,7 +6,6 @@ import Filter from '../../components/Filter/Filter';
 const ContactsPage = () => {
   return (
     <>
-      {/* <h1>Phonebook</h1> */}
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
